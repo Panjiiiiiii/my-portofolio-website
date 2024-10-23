402ad07290d8d28a24e4d84a80159441657cc853
Panjiiiiiii <110644169+Panjiiiiiii@users.noreply.github.com>
2024-10-24 06:01:33 +0700

    fixing grid

--- src/components/ImageCarousel.tsx
+++ src/components/ImageCarousel.tsx
@@ -72,7 +72,7 @@ const PortoSection: React.FC = () => {
           A collection of some projects I've worked on.
         </p>
 
-        <div className="grid grid-cols-1 gap-8 mt-8 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        <div className="grid grid-cols gap-8 mt-8 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3">
           {cards.map((card, index) => (
             <div
               key={index}
